da3fb63112d9f38735c3d78dbefb7f9b8c47a0c8
duy <ddao0892@gmail.com>
2021-01-26 12:28:21 -0700

    finished insertion-sort

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,8 +1,55 @@
-// Implement Insertion Sort
+// procedure insertionSort( A : array of items )
+//    int holePosition
+//    int valueToInsert
 
+//    for i = 1 to length(A) inclusive do:
+
+//       /* select value to be inserted */
+//       valueToInsert = A[i]
+//       holePosition = i
+
+//       /*locate hole position for the element to be inserted */
+
+//       while holePosition > 0 and A[holePosition-1] > valueToInsert do:
+//          A[holePosition] = A[holePosition-1]
+//          holePosition = holePosition -1
+//       end while
+
+//       /* insert the number at hole position */
+//       A[holePosition] = valueToInsert
+
+//    end for
+
+// end procedure
+// function insertionSort(list) {
+//   for (let i = 1; i < list.length; i++) {
+//     let valueToInsert = list[i];
+//     let holePosition = i;
+//     for (var j = i - 1; j >= 0 && valueToInsert < list[j]; j--) {
+//       // let temp = list [holePosition];
+//       list [j] = list[j - 1]
+//     }
+//     list [j] = valueToInsert
+//   }
+
+//   return list
+// }
 function insertionSort(list) {
-  // your code here
+  for (let i = 1; i < list.length; i++) {
+    let valueToInsert = list[i];
+
+    let j = i - 1;
+    while(j >= 0 && valueToInsert < list[j])  {
+      list [j + 1] = list [j]
+      j--;
+    }
+    list [j + 1] = valueToInsert
+  }
+
+  return list
 }
+
+console.log(insertionSort([6,4,7,5,1,3,2,9,8]))
 
 module.exports = {
   insertionSort
